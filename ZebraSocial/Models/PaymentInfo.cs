4e00f0d281aac1e3406a5bc23438284e5315df49
Jonathon Joyner <jonathonandon@gmail.com>
2021-04-13 20:31:53 -0500

    add payment info

---
+++ ZebraSocial/Models/PaymentInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZebraSocial.Models
+{
+    public class PaymentInfo
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Card_Number { get; set; }
+        public string Exp_Month { get; set; }
+        public int Exp_Year { get; set; }
+        public int CVV { get; set; }
+    }
+}
