38afdc1959e8608e7badeda9096420dbd11a0c7a
Jordan Smith <jordansmith989@gmail.com>
2021-04-15 19:42:48 -0500

    saving before merging

---
+++ ZebraSocial/DataAccess/EventsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ZebraSocial.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace ZebraSocial.DataAccess
+{
+    public class EventsRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=ZebraSocial;Trusted_Connection=True;";
+
+        public List<Event>GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+            var sql = @"SELECT *
+                        FROM Events";
+            var results = db.Query<Event>(sql).ToList();
+            return results;
+        }
+    }
+}
