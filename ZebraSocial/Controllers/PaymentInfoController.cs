5b52df5ed796d9f7a0c7ef025959635e24e70070
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2021-04-17 10:24:06 -0500

    Can now view all Payments

--- ZebraSocial/Controllers/PaymentInfoController.cs
+++ ZebraSocial/Controllers/PaymentInfoController.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZebraSocial.Models;
+using ZebraSocial.DataAccess;
+using Dapper;
 
 namespace ZebraSocial.Controllers
 {
@@ -11,5 +14,19 @@ namespace ZebraSocial.Controllers
     [ApiController]
     public class PaymentInfoController : ControllerBase
     {
+        PaymentInfoRepository _repo;
+
+        public PaymentInfoController()
+        {
+            _repo = new PaymentInfoRepository();
+        }
+
+        //Get api PaymentInfo
+        [HttpGet]
+        public IActionResult GetAllPaymentInfos()
+        {
+            return Ok(_repo.GetAll());
+        }
+
     }
 }
---
+++ ZebraSocial/DataAccess/PaymentInfoRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZebraSocial.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+
+namespace ZebraSocial.DataAccess
+{
+    public class PaymentInfoRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=ZebraSocial;Trusted_Connection=True;";
+
+        //GET ALL Payments
+        public List<PaymentInfo> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"Select *
+                        From PaymentInfo";
+
+            return db.Query<PaymentInfo>(sql).ToList();
+        }
+    }
+}
