4aa8904cf37880ab3c71c146b11364aa7433314f
Jordan Smith <jordansmith989@gmail.com>
2021-04-20 20:32:09 -0500

    CORS

--- ZebraSocial/Startup.cs
+++ ZebraSocial/Startup.cs
@@ -34,6 +34,7 @@ namespace ZebraSocial
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             }
 
             app.UseHttpsRedirection();
