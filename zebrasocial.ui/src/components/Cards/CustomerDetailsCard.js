06e67e3ea918422e81b699d5de0928f551e5b14b
Jordan Smith <jordansmith989@gmail.com>
2021-04-23 01:00:19 -0500

    payment info added to card

--- zebrasocial.ui/src/components/Cards/CustomerDetailsCard.js
+++ zebrasocial.ui/src/components/Cards/CustomerDetailsCard.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function CustomerDetailsCard({ customer }) {
+export default function CustomerDetailsCard({ customer, payment }) {
   return (
-    <div className='card customerDetails m-2 border-light col-md-6' >
-        <h1 className='card-title m-2'>Customer Information</h1>
-      <div className='card-body'>
-            <img className='card-img-top rounded-pill border border-white' src={customer.imageUrl} alt='Card cap' />
-            <p className='card-text'>Name: {customer.firstName} {customer.lastName}</p>
-            <p className='card-text'>Email: {customer.email}</p>
-            <p className='card-text'>Payment Id: {customer.paymentId}</p>
-      </div>
+      <div className="profile">
+    <div className="page-content page-container" id="page-content">
+    <div className="padding">
+        <div className="row container d-flex justify-content-center">
+            <div className="col-xl-6 col-md-12">
+                <div className="card user-card-full">
+                    <div className="row m-l-0 m-r-0">
+                        <div className="col-sm-4 bg-c-lite-green user-profile">
+                            <div className="card-block text-center text-white">
+                                <div className="m-b-25"> <img src={customer.imageUrl} className="img-radius face" alt="User-Profile-Image"/> </div>
+                                <h3 className="f-w-600">{customer.firstName} {customer.lastName}</h3>
+                            </div>
+                        </div>
+                        <div className="col-sm-8">
+                            <div className="card-block">
+                                <h6 className="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <p className="m-b-10 f-w-600">Event</p>
+                                        <h6 className="text-muted f-w-400">Event Info Here</h6>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="m-b-10 f-w-600">Email</p>
+                                        <h6 className="text-muted f-w-400">{customer.email}</h6>
+                                    </div>
+                                </div>
+                                <h6 className="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Payment Info</h6>
+                                <div className="row">
+                                    <div className="col-sm-6">
+                                        <p className="m-b-10 f-w-600">Name On Card</p>
+                                        <h6 className="text-muted f-w-400"><b>First:</b> {payment.firstName}</h6>
+                                        <h6 className="text-muted f-w-400"><b>Last:</b> {payment.lastName}</h6>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <p className="m-b-10 f-w-600">Card Info</p>
+                                        <h6 className="text-muted f-w-400">{payment.cardNumber}</h6>
+                                        <h6 className="text-muted f-w-400"><b>Month: </b>{payment.expMonth} | <b>Year:</b> {payment.expYear}</h6>
+                                        <h6 className="text-muted f-w-400">CVV: {payment.cvv}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+</div>
   );
 }
 
 CustomerDetailsCard.propTypes = {
-  customer: PropTypes.any
+  customer: PropTypes.any,
+  payment: PropTypes.any
 };
--- zebrasocial.ui/src/views/CustomerDetails.js
+++ zebrasocial.ui/src/views/CustomerDetails.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import getCustomersById from '../helpers/data/customerData';
+import getPaymentInfoById from '../helpers/data/paymentData';
 import CustomerDetailsCard from '../components/Cards/CustomerDetailsCard';
 
 const CustomerDetails = (props) => {
   const [customer, setCustomers] = useState([]);
+  const [payment, setPayments] = useState([]);
 
   // const getCustomers = () => {
   //   getCustomersById.getAllCustomers().then((response) => {
@@ -20,14 +22,22 @@ const CustomerDetails = (props) => {
     })
       .catch((err) => console.warn('nope', err));
   };
+  const getPayment = (customerId) => {
+    getPaymentInfoById.getPaymentInfoById(customerId).then((response) => {
+      const paymentInfo = response;
+      setPayments(paymentInfo);
+    })
+      .catch((err) => console.warn('nope', err));
+  };
 
   useEffect(() => {
     const customerId = props.match.params.id;
     getCustomer(customerId);
+    getPayment(customerId);
   }, []);
 
   const showCustomers = () => (
-     <CustomerDetailsCard key={customer.id} customer={customer} />
+     <CustomerDetailsCard key={customer.id} customer={customer} payment={payment} />
   );
 
   return (
