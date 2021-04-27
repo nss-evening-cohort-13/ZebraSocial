9ae2c7d9c3424025285f3a182dad46c3b7c01d50
Jordan Smith <jordansmith989@gmail.com>
2021-04-26 20:56:52 -0500

    able to update info

--- zebrasocial.ui/src/components/Forms/EditCustomerForm.js
+++ zebrasocial.ui/src/components/Forms/EditCustomerForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
+import updateCustomerInfo from '../../helpers/data/customerData';
 
 export default function EditCustomerForm({ customer }) {
   const {
@@ -8,22 +9,44 @@ export default function EditCustomerForm({ customer }) {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log('data', data);
+  // const onSubmit = (data) => console.log('data', data);
 
-  //   console.log(watch('example')); // watch input value by passing the name of it
+  const onSubmit = (data) => {
+    const cust = customer.id;
+    const parsedId = Number(data.id);
+    const parsedPayment = Number(data.paymentId);
+    const dataObject = {
+      id: parsedId,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      imageUrl: data.imageUrl,
+      paymentId: parsedPayment
+    };
+    updateCustomerInfo.updateCustomerInfo(cust, dataObject)
+      .catch((err) => console.warn('nope', err));
+  };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue={customer.name} {...register('example')} />
+      <h5>Customer Id(read only)</h5>
+      <input defaultValue={customer.id} {...register('id')} readOnly/>
+      <h5>First Name</h5>
+      <input defaultValue={customer.firstName} {...register('firstName', { required: true })} />
+      <h5>Last Name</h5>
+      <input defaultValue={customer.lastName} {...register('lastName', { required: true })} />
+      <h5>Email</h5>
+      <input defaultValue={customer.email} {...register('email', { required: true })} />
+      <h5>Image Url</h5>
+      <input defaultValue={customer.imageUrl} {...register('imageUrl', { required: true })} />
+      <h5>Payment Id(read only)</h5>
+      <input defaultValue={customer.paymentId} {...register('paymentId')} readOnly/>
+      {errors.firstName && <span>This field is required</span>}
+      {errors.lastName && <span>This field is required</span>}
+      {errors.email && <span>This field is required</span>}
+      {errors.imageUrl && <span>This field is required</span>}
 
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register('exampleRequired', { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type='submit' />
+      <button type='submit'>Submit</button>
     </form>
   );
 }
