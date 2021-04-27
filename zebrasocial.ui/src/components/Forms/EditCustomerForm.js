c863281f51faeff50ffc81957b6cb56403bc0dc3
Jordan Smith <jordansmith989@gmail.com>
2021-04-26 23:31:22 -0500

    hid some inputs

--- zebrasocial.ui/src/components/Forms/EditCustomerForm.js
+++ zebrasocial.ui/src/components/Forms/EditCustomerForm.js
@@ -29,8 +29,7 @@ export default function EditCustomerForm({ customer }) {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <h5>Customer Id(read only)</h5>
-      <input defaultValue={customer.id} {...register('id')} readOnly/>
+      <input type="hidden" defaultValue={customer.id} {...register('id')} readOnly/>
       <h5>First Name</h5>
       <input defaultValue={customer.firstName} {...register('firstName', { required: true })} />
       <h5>Last Name</h5>
@@ -39,8 +38,7 @@ export default function EditCustomerForm({ customer }) {
       <input defaultValue={customer.email} {...register('email', { required: true })} />
       <h5>Image Url</h5>
       <input defaultValue={customer.imageUrl} {...register('imageUrl', { required: true })} />
-      <h5>Payment Id(read only)</h5>
-      <input defaultValue={customer.paymentId} {...register('paymentId')} readOnly/>
+      <input type="hidden" defaultValue={customer.paymentId} {...register('paymentId')} readOnly/>
       {errors.firstName && <span>This field is required</span>}
       {errors.lastName && <span>This field is required</span>}
       {errors.email && <span>This field is required</span>}
