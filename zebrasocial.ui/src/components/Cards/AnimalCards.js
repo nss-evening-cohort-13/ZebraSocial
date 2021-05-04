d28949a1e59598467eb7375c1339b7d8085a2622
Deanna <dmariemix93@gmail.com>
2021-05-04 18:16:20 -0500

    added modal with zebra details

--- zebrasocial.ui/src/components/Cards/AnimalCards.js
+++ zebrasocial.ui/src/components/Cards/AnimalCards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MyModal from '../AppModal/index';
 
 export default function AnimalsCard({ animal }) {
   return (
@@ -13,6 +14,16 @@ export default function AnimalsCard({ animal }) {
           <h5>({animal.id})</h5>
         </p>
       </div>
+      <MyModal title={'About This Rental'} buttonLabel={'Click Here For Details'}>
+      <div className='card-body embed-responsive embed-responsive-16by9'>
+        <img src={animal.imageUrl} className='image embed-responsive-item'/>
+      </div>
+        <h4 className='card-info'>Name: {animal.name}</h4>
+        <h4 className='card-title'>Type: {animal.type}</h4>
+        <h4 className='card-title'>Can Be Used For: {animal.eventSpecialty}</h4>
+        <h4 className='card-title'>Price: {animal.price}</h4>
+        <h4 className='card-title'>Description: {animal.description}</h4>
+      </MyModal>
     </div>
 </div>
   );
