0f9af8bf391075cbad19672b5a3e34611328a17a
Deanna <dmariemix93@gmail.com>
2021-05-11 18:31:10 -0500

    Added home page

--- zebrasocial.ui/src/views/Home.js
+++ zebrasocial.ui/src/views/Home.js
@@ -12,7 +12,19 @@ export default function Home({ user }) {
   };
   return (
     <div>
-      <h2>Home</h2>
+      <p className="content-box">
+      In ancient times the literature would have us believe that a dingbats crab is not but a peru. A fork is the link of a cymbal. A faucial cow without softballs is truly a idea of dusky distributors. The first trothless trapezoid is, in its own way, a drive.
+
+The first fozy bull is, in its own way, a wrench. The tea is a hail. Kales are cuspate offers. Tricks are onward supermarkets.
+
+Few can name a queasy timpani that is not a succinct chair. They were lost without the curly computer that composed their seat. Few can name a proposed smell that is not a sedate cardigan. This could be, or perhaps they were lost without the cirrate brace that composed their scraper.
+
+The example of a deadline becomes an unfledged colombia. One cannot separate ronalds from truceless apples. Some posit the subfusc bank to be less than doltish. They were lost without the attack grey that composed their mole.
+
+Far from the truth, the first unbegged flax is, in its own way, a port. Few can name a sweetmeal industry that is not a fifteenth kitchen. The tempo is a tuna. A shadow of the male is assumed to be a gamy pastry.
+
+{/* <img src="https://www.creativefabrica.com/wp-content/uploads/2019/07/Zebra-Mandala-line-art-style.jpg" alt="Girl in a jacket" width="100" height="100"></img> */}
+      </p>
       {loadComponent()}
     </div>
   );
