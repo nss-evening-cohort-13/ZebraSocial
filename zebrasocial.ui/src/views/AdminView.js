0b0c2a257f7ca6246208fa5a31aa48f63632f90c
Jordan Smith <jordansmith989@gmail.com>
2021-05-14 01:03:56 -0500

    admin dashboard mostly done

--- zebrasocial.ui/src/views/AdminView.js
+++ zebrasocial.ui/src/views/AdminView.js
@@ -5,9 +5,12 @@ import { Button } from 'reactstrap';
 export default function AdminView() {
   return (
         <div>
-            <Link to={'/products'} className='admin-animals text-muted f-w-400'><Button color="info">View Zebras</Button></Link>
-            <Link to={'/events'} className='admin-animals text-muted f-w-400'><Button color="success">View Events</Button></Link>
-            <Link to={'/customers'} className='admin-animals text-muted f-w-400'><Button color="danger">View Customers</Button></Link>
+          <h2>Admin Dashboard</h2>
+          <div className="adminDash">
+            <Link to={'/products'} className='admin-animals text-muted f-w-400 p-4'><Button color="info">View Zebras</Button></Link>
+            <Link to={'/events'} className='admin-animals text-muted f-w-400 p-4'><Button color="success">View Events</Button></Link>
+            <Link to={'/customers'} className='admin-animals text-muted f-w-400 p-4'><Button color="danger">View Customers</Button></Link>
+            </div>
         </div>
   );
 }
