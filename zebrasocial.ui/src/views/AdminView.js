6c22a5682936a8dd145edef086076574dc1924a5
Jordan Smith <jordansmith989@gmail.com>
2021-05-15 10:09:06 -0500

    saving because computer doing weird shit

--- zebrasocial.ui/src/views/AdminView.js
+++ zebrasocial.ui/src/views/AdminView.js
@@ -6,7 +6,33 @@ export default function AdminView() {
   return (
         <div>
           <h2>Admin Dashboard</h2>
-          <div className="adminDash">
+            <div className="counter-container">
+          <div className="container">
+    <div className="row">
+        <div className="four col-md-3">
+            <div className="counter-box colored"> <i className="fa fa-thumbs-o-up"></i> <span className="counter">2147</span>
+                <p>Happy Customers</p>
+            </div>
+        </div>
+        <div className="four col-md-3">
+            <div className="counter-box"> <i className="fa fa-group"></i> <span className="counter">3275</span>
+                <p>Registered Members</p>
+            </div>
+        </div>
+        <div className="four col-md-3">
+            <div className="counter-box"> <i className="fa fa-shopping-cart"></i> <span className="counter">289</span>
+                <p>Available Products</p>
+            </div>
+        </div>
+        <div className="four col-md-3">
+            <div className="counter-box"> <i className="fa fa-user"></i> <span className="counter">1563</span>
+                <p>Saved Trees</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+            <div className="adminDash">
             <Link to={'/products'} className='admin-animals text-muted f-w-400 p-4'><Button color="info">View Zebras</Button></Link>
             <Link to={'/events'} className='admin-animals text-muted f-w-400 p-4'><Button color="success">View Events</Button></Link>
             <Link to={'/customers'} className='admin-animals text-muted f-w-400 p-4'><Button color="danger">View Customers</Button></Link>
